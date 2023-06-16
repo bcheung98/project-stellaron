babc7453e05ee871e4813a590e1e68412d139f10
Brian <briantcheung@gmail.com>
2023-06-15 18:39:07 -0700

    Refactored trace components

--- src/components/characters/page/CharacterTraceDisplay.js
+++ src/components/characters/page/CharacterTraceDisplay.js
@@ -1,8 +1,43 @@
 import * as React from "react";
 import { useTheme } from "@mui/material/styles";
-import parse from "html-react-parser";
-import { Typography, Box, AppBar, CardHeader, Avatar } from "@mui/material";
+import { Typography, Box, AppBar } from "@mui/material";
 import Xarrow from "react-xarrows";
+import { TraceNodeMain, TraceNodeSmall } from "../../../helpers/TraceNode";
+
+// Recursively displays all trace nodes
+const ShowTraces = (props) => {
+
+    let { name, traces, id } = props;
+
+    return (
+        <Box sx={{ display: "flex", alignItems: "center" }} >
+            {
+                traces.name ?
+                    <TraceNodeMain id={id} name={name} traces={traces} />
+                    :
+                    <TraceNodeSmall id={id} traces={traces} />
+            }
+            <Box>
+                {
+                    traces?.subTraces?.map((trace, index) => {
+                        let nextID = incrementID(id);
+                        // If there is more than one child node, add an extra identifier to the ID
+                        if (traces.subTraces?.length > 1) {
+                            nextID = nextID + `-${index}`;
+                        }
+                        return (
+                            <Box key={index} sx={{ my: "15px" }}>
+                                <ShowTraces name={name} traces={trace} id={nextID} />
+                                <Xarrow start={id} end={nextID} showHead={false} path="grid" color="lightgray" strokeWidth={3} />
+                            </Box>
+                        )
+                    })
+                }
+            </Box>
+        </Box>
+    )
+
+}
 
 const CharacterTraceDisplay = (props) => {
 
@@ -47,131 +82,6 @@ const CharacterTraceDisplay = (props) => {
 }
 
 export default CharacterTraceDisplay;
-
-// Recursively displays all trace nodes
-const ShowTraces = (props) => {
-
-    const theme = useTheme();
-
-    let { name, traces, id } = props;
-
-    return (
-        <Box sx={{ display: "flex", alignItems: "center" }} >
-            {
-                traces.name ?
-                    // Component for the bonus abilities
-                    <Box
-                        sx={{
-                            backgroundColor: `${theme.table.body.backgroundColor}`,
-                            border: `1px solid ${theme.border.color}`,
-                            borderRadius: "5px",
-                            pr: "15px",
-                            py: "10px",
-                            width: "30%",
-                            mr: "80px",
-                        }}
-                        id={id}
-                    >
-                        <CardHeader
-                            sx={{
-                                display: "flex",
-                                alignItems: "center",
-                            }}
-                            avatar={
-                                <Avatar alt={traces.name} src={(`${process.env.REACT_APP_URL}/characters/traces/${name.split(" ").join("_").toLowerCase()}_${traces.unlock.toLowerCase()}.webp`)}
-                                    sx={{
-                                        width: "56px",
-                                        height: "56px",
-                                        border: `2px solid ${theme.border.color}`,
-                                    }}
-                                >
-                                    <img src={`${process.env.REACT_APP_URL}/Unknown.png`} alt="Unknown" style={{ width: "56px", backgroundColor: `${theme.paper.backgroundColor}` }} />
-                                </Avatar>
-                            }
-                            title={
-                                <React.Fragment>
-                                    <Typography variant="h6" sx={{ color: `${theme.text.color}`, fontWeight: "450" }}>
-                                        {traces.name}
-                                    </Typography>
-                                    <Typography variant="subtitle1" sx={{ color: `${theme.text.color}`, fontWeight: "450" }}>
-                                        <i>{traces.unlock}</i>
-                                    </Typography>
-                                </React.Fragment>
-                            }
-                        />
-                        <Typography variant="body1" sx={{ color: `${theme.text.color}`, ml: "20px" }}>
-                            {parse(traces.description)}
-                        </Typography>
-                    </Box>
-                    :
-                    // Component for the extra trace nodes
-                    <React.Fragment>
-                        {
-                            traces.description &&
-                            <Box
-                                sx={{
-                                    backgroundColor: `${theme.table.body.backgroundColor}`,
-                                    border: `1px solid ${theme.border.color}`,
-                                    borderRadius: "5px",
-                                    mr: "60px",
-                                }}
-                                id={id}
-                            >
-                                <CardHeader
-                                    sx={{
-                                        display: "flex",
-                                        alignItems: "center",
-                                    }}
-                                    avatar={
-                                        <Avatar alt={traces.type} src={(`${process.env.REACT_APP_URL}/stat_icons/${traces.type.split(" ").join("_")}.webp`)}
-                                            sx={{
-                                                width: "48px",
-                                                height: "48px",
-                                                border: `2px solid ${theme.border.color}`,
-                                            }}
-                                        >
-                                            <img src={`${process.env.REACT_APP_URL}/Unknown.png`} alt="Unknown" style={{ width: "48px", backgroundColor: `${theme.paper.backgroundColor}` }} />
-                                        </Avatar>
-                                    }
-                                    title={
-                                        <React.Fragment>
-                                            <Typography variant="body1" sx={{ color: `${theme.text.color}`, fontWeight: "450" }}>
-                                                {parse(traces.description)}
-                                            </Typography>
-                                            <Typography variant="subtitle1" sx={{ color: `${theme.text.color}`, fontWeight: "450" }}>
-                                                <i>{traces.unlock}</i>
-                                            </Typography>
-                                        </React.Fragment>
-                                    }
-                                />
-                            </Box>
-                        }
-                    </React.Fragment>
-            }
-            <Box>
-                {
-                    traces?.subTraces?.map((trace, index) => {
-                        let nextID = "";
-                        // If there is more than one child node, add an extra identifier to the ID
-                        if (traces.subTraces?.length > 1) {
-                            nextID = incrementID(id) + `-${index}`;
-                        }
-                        else {
-                            nextID = incrementID(id);
-                        }
-                        return (
-                            <Box key={index} sx={{ my: "15px" }}>
-                                <ShowTraces name={name} traces={trace} id={nextID} />
-                                <Xarrow start={id} end={nextID} showHead={false} path="grid" color="lightgray" strokeWidth={3} />
-                            </Box>
-                        )
-                    })
-                }
-            </Box>
-        </Box>
-    )
-
-}
 
 const incrementID = (id) => {
     let splitID = id.split("-")
---
+++ src/helpers/TraceNode.js
@@ -0,0 +1,108 @@
+import * as React from "react";
+import { useTheme } from "@mui/material/styles";
+import parse from "html-react-parser";
+import { Typography, Box, CardHeader, Avatar } from "@mui/material";
+
+// Component for the main bonus abilities
+export const TraceNodeMain = (props) => {
+
+    const theme = useTheme();
+
+    let { id, name, traces } = props;
+
+    return (
+        <Box
+            sx={{
+                backgroundColor: `${theme.table.body.backgroundColor}`,
+                border: `1px solid ${theme.border.color}`,
+                borderRadius: "5px",
+                pr: "15px",
+                py: "10px",
+                width: "30%",
+                mr: "80px",
+            }}
+            id={id}
+        >
+            <CardHeader
+                sx={{
+                    display: "flex",
+                    alignItems: "center",
+                }}
+                avatar={
+                    <Avatar alt={traces.name} src={(`${process.env.REACT_APP_URL}/characters/traces/${name.split(" ").join("_").toLowerCase()}_${traces.unlock.toLowerCase()}.webp`)}
+                        sx={{
+                            width: "56px",
+                            height: "56px",
+                            border: `2px solid ${theme.border.color}`,
+                        }}
+                    >
+                        <img src={`${process.env.REACT_APP_URL}/Unknown.png`} alt="Unknown" style={{ width: "56px", backgroundColor: `${theme.paper.backgroundColor}` }} />
+                    </Avatar>
+                }
+                title={
+                    <React.Fragment>
+                        <Typography variant="h6" sx={{ color: `${theme.text.color}`, fontWeight: "450" }}>
+                            {traces.name}
+                        </Typography>
+                        <Typography variant="subtitle1" sx={{ color: `${theme.text.color}`, fontWeight: "450" }}>
+                            <i>{traces.unlock}</i>
+                        </Typography>
+                    </React.Fragment>
+                }
+            />
+            <Typography variant="body1" sx={{ color: `${theme.text.color}`, ml: "20px" }}>
+                {parse(traces.description)}
+            </Typography>
+        </Box>
+    )
+
+}
+
+// Component for the extra trace nodes
+export const TraceNodeSmall = (props) => {
+
+    const theme = useTheme();
+
+    let { id, traces } = props;
+
+    return (
+        <Box
+            sx={{
+                backgroundColor: `${theme.table.body.backgroundColor}`,
+                border: `1px solid ${theme.border.color}`,
+                borderRadius: "5px",
+                mr: "80px",
+            }}
+            id={id}
+        >
+            <CardHeader
+                sx={{
+                    display: "flex",
+                    alignItems: "center",
+                }}
+                avatar={
+                    <Avatar alt={traces.type} src={(`${process.env.REACT_APP_URL}/stat_icons/${traces.type.split(" ").join("_")}.webp`)}
+                        sx={{
+                            width: "36px",
+                            height: "36px",
+                            border: `2px solid ${theme.border.color}`,
+                        }}
+                    >
+                        <img src={`${process.env.REACT_APP_URL}/Unknown.png`} alt="Unknown" style={{ width: "48px", backgroundColor: `${theme.paper.backgroundColor}` }} />
+                    </Avatar>
+                }
+                title={
+                    <React.Fragment>
+                        <Typography variant="body1" sx={{ color: `${theme.text.color}`, fontWeight: "450" }}>
+                            {parse(traces.description)}
+                        </Typography>
+                        <Typography variant="subtitle1" sx={{ color: `${theme.text.color}`, fontWeight: "450" }}>
+                            <i>{traces.unlock}</i>
+                        </Typography>
+                    </React.Fragment>
+                }
+            />
+        </Box>
+    )
+
+}
