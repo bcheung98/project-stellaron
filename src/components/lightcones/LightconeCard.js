994cd3a20ce97ee08fa3831e02e68c733c00fbb0
Brian <briantcheung@gmail.com>
2023-06-21 23:31:05 -0700

    Added image, rarity, and stats to lightcone card

--- src/components/lightcones/LightconeCard.js
+++ src/components/lightcones/LightconeCard.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { useTheme } from "@mui/material/styles";
-import { Typography, Card, CardContent, ButtonBase, Box } from "@mui/material";
+import { Typography, Avatar, Card, CardHeader, CardMedia, CardContent, ButtonBase, Box } from "@mui/material";
 import { CustomTooltip } from "../../helpers/CustomTooltip";
 import ErrorLoadingImage from "../../helpers/ErrorLoadingImage";
 
@@ -8,24 +8,20 @@ const LightconeCard = (props) => {
 
     const theme = useTheme();
 
-    let { name, path, rarity } = props.lightcone;
+    let { name, path, rarity, stats } = props.lightcone;
 
-    const smallIcon = {
+    const StatIcon = {
         width: "30px",
         height: "30px",
-        backgroundColor: `${theme.materialImage.backgroundColor}`,
-        border: `1px solid ${theme.border.color}`,
-        borderRadius: "15px",
-        marginTop: "5px",
-        marginRight: "10px",
-    }
+        mr: "-10px",
+    };
 
     return (
         <React.Fragment>
             <Card variant="outlined"
                 sx={{
-                    width: "350px",
-                    height: "150px",
+                    width: "365px",
+                    height: "215px",
                     mx: "auto",
                     my: "10px",
                     backgroundColor: `${theme.card.backgroundColor}`,
@@ -33,16 +29,83 @@ const LightconeCard = (props) => {
                     borderRadius: "5px",
                 }}
             >
-                <CardContent sx={{ py: "10px" }}>
-                    <Box sx={{ display: "flex" }}>
-                        <CustomTooltip title={path} arrow placement="top">
-                            <img style={smallIcon} src={(`${process.env.REACT_APP_URL}/paths/Path_The_${path}.png`)} alt={path} onError={ErrorLoadingImage} />
-                        </CustomTooltip>
-                        <ButtonBase disableRipple>
+                <ButtonBase disableRipple>
+                    <CardHeader
+                        avatar={
+                            <CustomTooltip title={path} arrow placement="top">
+                                <Avatar src={(`${process.env.REACT_APP_URL}/paths/Path_The_${path}.png`)} alt={path}
+                                    sx={{
+                                        width: "30px",
+                                        height: "30px",
+                                        backgroundColor: `${theme.card.backgroundColor}`,
+                                        border: `1px solid ${theme.border.color}`,
+                                        borderRadius: "15px",
+                                    }}
+                                >
+                                    <img src={`${process.env.REACT_APP_URL}/images/Unknown.png`} alt="Unknown" style={{ width: "30px" }} />
+                                </Avatar>
+                            </CustomTooltip>
+                        }
+                        title={
                             <Typography sx={{ color: `${theme.text.color}`, fontWeight: "bold" }} variant="h5">
                                 {name}
                             </Typography>
-                        </ButtonBase>
+                        }
+                    />
+                </ButtonBase>
+                <CardMedia
+                    component="img"
+                    image={`${process.env.REACT_APP_URL}/lightcones/artwork/${name}.webp`}
+                    alt={name}
+                    sx={{ height: "110px", objectFit: "cover", objectPosition: "100% 40%" }}
+                    onError={ErrorLoadingImage}
+                />
+                <CardContent>
+                    <Box sx={{ display: "flex", mt: "-12px" }}>
+                        <Typography sx={{ color: "rgb(255, 208, 112)", fontWeight: "bolder" }} variant="h5">
+                            {[...Array(rarity).keys()].map(() => "✦")}
+                        </Typography>
+                        <Box sx={{ display: "flex", mt: "-16px", ml: "15px" }}>
+                            <CardHeader
+                                avatar={
+                                    <Avatar src={(`${process.env.REACT_APP_URL}/icons/Icon_HP.png`)} alt={path} sx={StatIcon}>
+                                        <img src={`${process.env.REACT_APP_URL}/images/Unknown.png`} alt="Unknown" style={{ width: "30px" }} />
+                                    </Avatar>
+                                }
+                                title={
+                                    <Typography sx={{ color: `${theme.text.color}`, fontWeight: "bold" }} variant="h6">
+                                        {stats.hp}
+                                    </Typography>
+                                }
+                                sx={{ mr: "-15px" }}
+                            />
+                            <CardHeader
+                                avatar={
+                                    <Avatar src={(`${process.env.REACT_APP_URL}/icons/Icon_ATK.png`)} alt={path} sx={StatIcon}>
+                                        <img src={`${process.env.REACT_APP_URL}/images/Unknown.png`} alt="Unknown" style={{ width: "30px" }} />
+                                    </Avatar>
+                                }
+                                title={
+                                    <Typography sx={{ color: `${theme.text.color}`, fontWeight: "bold" }} variant="h6">
+                                        {stats.atk}
+                                    </Typography>
+                                }
+                                sx={{ mr: "-15px" }}
+                            />
+                            <CardHeader
+                                avatar={
+                                    <Avatar src={(`${process.env.REACT_APP_URL}/icons/Icon_DEF.png`)} alt={path} sx={StatIcon}>
+                                        <img src={`${process.env.REACT_APP_URL}/images/Unknown.png`} alt="Unknown" style={{ width: "30px" }} />
+                                    </Avatar>
+                                }
+                                title={
+                                    <Typography sx={{ color: `${theme.text.color}`, fontWeight: "bold" }} variant="h6">
+                                        {stats.def}
+                                    </Typography>
+                                }
+                                sx={{ mr: "-15px" }}
+                            />
+                        </Box>
                     </Box>
                 </CardContent>
             </Card>
