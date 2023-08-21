c0b85039558307f9de02d3656fd555ada6fbc3d3
Brian <briantcheung@gmail.com>
2023-08-20 23:58:11 -0700

    Adjusted slider size

--- src/components/lightcones/page/_LightconePage.js
+++ src/components/lightcones/page/_LightconePage.js
@@ -128,7 +128,7 @@ const LightconePage = (props) => {
                             <Typography sx={{ color: `${theme.text.color}`, mt: "10px" }} variant="body1">
                                 {parse(passive.effect)}
                             </Typography>
-                            <Box sx={{ display: "flex", alignItems: "center", width: "50%", mt: "15px" }}>
+                            <Box sx={{ display: "flex", alignItems: "center", width: "20%", mt: "15px" }}>
                                 <Typography variant="h6" sx={{ fontWeight: "bold", color: `${theme.text.color}`, mr: "35px" }}>
                                     S{sliderValue}
                                 </Typography>
