78f0a2d9fde072c2535fd49aaaefa5bc7a26d245
Brian <briantcheung@gmail.com>
2023-10-25 23:50:22 -0700

    Added tooltips for character banner

--- src/components/banners/CharacterBannerRow.js
+++ src/components/banners/CharacterBannerRow.js
@@ -3,6 +3,7 @@ import { useTheme } from "@mui/material/styles";
 import { StyledTableCell } from "../../helpers/CustomTable";
 import { Box } from "@mui/system";
 import { Typography, ButtonBase, Avatar, TableRow } from "@mui/material";
+import { CustomTooltip } from "../../helpers/CustomTooltip";
 import ErrorLoadingImage from "../../helpers/ErrorLoadingImage";
 
 const CharIconBackground = (index, len) => {
@@ -66,27 +67,29 @@ const CharacterBannerRow = (props) => {
                             {
                                 row.banner.map((char, index) => (
                                     <ButtonBase disableRipple href={`/project-stellaron/character/${char.split(" ").join("_").toLowerCase()}`} target="_blank" key={char} sx={{ m: "2px" }}>
-                                        <Avatar variant="square" src={`${process.env.REACT_APP_URL}/characters/icons/Icon_${char.split(" ").join("_")}.png`} alt={char}
-                                            sx={{
-                                                mx: "2px",
-                                                border: `2px solid ${theme.border.color}`,
-                                                borderRadius: "64px",
-                                                width: "64px",
-                                                height: "64px",
-                                                backgroundColor: `${theme.materialImage.backgroundColor}`,
-                                            }}
-                                            style={CharIconBackground(index, row.banner.length)}
-                                        >
-                                            <img src={`${process.env.REACT_APP_URL}/characters/icons/Icon_${char.split(" ").join("_")}.png`} alt={char} onError={ErrorLoadingImage}
-                                                style={{
+                                        <CustomTooltip title={char} arrow placement="top">
+                                            <Avatar variant="square" src={`${process.env.REACT_APP_URL}/characters/icons/Icon_${char.split(" ").join("_")}.png`} alt={char}
+                                                sx={{
                                                     mx: "2px",
-                                                    border: `1px solid ${theme.border.color}`,
+                                                    border: `2px solid ${theme.border.color}`,
                                                     borderRadius: "64px",
                                                     width: "64px",
                                                     height: "64px",
+                                                    backgroundColor: `${theme.materialImage.backgroundColor}`,
                                                 }}
-                                            />
-                                        </Avatar>
+                                                style={CharIconBackground(index, row.banner.length)}
+                                            >
+                                                <img src={`${process.env.REACT_APP_URL}/characters/icons/Icon_${char.split(" ").join("_")}.png`} alt={char} onError={ErrorLoadingImage}
+                                                    style={{
+                                                        mx: "2px",
+                                                        border: `1px solid ${theme.border.color}`,
+                                                        borderRadius: "64px",
+                                                        width: "64px",
+                                                        height: "64px",
+                                                    }}
+                                                />
+                                            </Avatar>
+                                        </CustomTooltip>
                                     </ButtonBase>
                                 ))
                             }
