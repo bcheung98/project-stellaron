8c9e395239ff8f595eb34e2ae35a6207f7609b2e
Brian <briantcheung@gmail.com>
2024-02-09 02:51:27 -0800

    Added comment line

--- src/redux/actions/fetchLightcones.js
+++ src/redux/actions/fetchLightcones.js
@@ -1,4 +1,5 @@
 export const fetchLightcones = () => {
+    // https://bcheung98.github.io/project-stellaron-db/lightcones.json
     const url = "https://bcheung98.github.io/project-stellaron-db/lightcones.json";
     return (dispatch) => {
         dispatch({ type: "START_GETTING_LC_REQUEST" });
