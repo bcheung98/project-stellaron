9ff29cc8c7a84cffeafc2e8fde11e85df567b15a
Brian <briantcheung@gmail.com>
2024-10-21 15:16:09 -0700

    Added custom components

---
+++ src/components/_custom/SearchBar.tsx
@@ -0,0 +1,64 @@
+import React from "react"
+import { useTheme, TextField } from "@mui/material"
+
+interface SearchBarProps {
+    onChange?: (event: React.BaseSyntheticEvent) => void,
+    value?: string,
+    placeholder?: string,
+    size?: {
+        width?: string,
+        height?: string
+    },
+    params?: any
+}
+
+function SearchBar({
+    onChange,
+    value,
+    placeholder = "Search",
+    size = {
+        width: "100%",
+        height: "100%"
+    },
+    params,
+}: SearchBarProps) {
+
+    const theme = useTheme()
+
+    return (
+        <TextField
+            {...params}
+            sx={{
+                "& .MuiOutlinedInput-root": {
+                    width: size.width,
+                    height: size.height,
+                    backgroundColor: `${theme.table.header.backgroundColor}`,
+                    color: `${theme.text.color}`,
+                    fontFamily: `${theme.font.styled.family}`,
+                    "& fieldset": {
+                        borderColor: `${theme.border.color}`,
+                        borderWidth: "1px",
+                        borderRadius: "5px",
+                    },
+                    "&:hover fieldset": {
+                        borderColor: `${theme.button.selected}`,
+                    },
+                    "&.Mui-focused fieldset": {
+                        borderColor: `${theme.button.selected}`,
+                    },
+                },
+                "& .MuiButtonBase-root": {
+                    color: `${theme.text.color}`,
+                }
+            }}
+            onChange={onChange}
+            value={value}
+            placeholder={placeholder}
+            fullWidth
+            autoComplete="off"
+        />
+    )
+
+}
+
+export default SearchBar
