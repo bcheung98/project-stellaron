ca905d2bf3da586f54322afb0453804de573e450
Brian <briantcheung@gmail.com>
2024-10-22 13:57:38 -0700

    Refactored function

---
+++ src/helpers/zoomImageOnHover.ts
@@ -0,0 +1,13 @@
+export default function zoomImageOnHover(direction: "enter" | "leave", id: string, translate = "translate(0px, 0px)") {
+    let image = document.getElementById(id)
+    if (image !== null) {
+        if (direction === "enter") {
+            image.style.transition = "all 100ms ease-in"
+            image.style.transform = `scale(1.1) ${translate}`
+        }
+        else {
+            image.style.transition = "all 100ms ease-out"
+            image.style.transform = `scale(1) ${translate}`
+        }
+    }
+}
