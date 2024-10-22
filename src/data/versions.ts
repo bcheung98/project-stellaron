f435f5b3e0fb45b29b30652d305123397b101068
Brian <briantcheung@gmail.com>
2024-10-21 19:36:03 -0700

    Moved versions to own file

---
+++ src/data/versions.ts
@@ -0,0 +1,17 @@
+// MAKE SURE TO CHANGE THIS EVERY UPDATE!
+export const updates = [
+    { version: "2.6", name: "Annals of Pinecany's Mappou Age" },
+    { version: "2.5", name: "Flying Aureus Shot to Lupine Rue" },
+    { version: "2.4", name: "Finest Duel Under the Pristine Blue" },
+    { version: "2.3", name: "Farewell, Penacony" },
+    { version: "2.2", name: "Then Wake to Weep" },
+    { version: "2.1", name: "Into the Yawning Chasm" },
+    { version: "2.0", name: "If One Dreams at Midnight" },
+    { version: "1.6", name: "Crown of the Mundane and Divine" },
+    { version: "1.5", name: "The Crepuscule Zone" },
+    { version: "1.4", name: "Jolted Awake From A Winter Dream" },
+    { version: "1.3", name: "Celestial Eyes Above Mortal Ruins" },
+    { version: "1.2", name: "Even Immortality Ends" },
+    { version: "1.1", name: "Galactic Roaming" },
+    { version: "1.0", name: "The Rail Unto the Stars" }
+]
