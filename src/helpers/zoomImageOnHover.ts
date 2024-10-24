d1e56124a6e36721e03d63a3b5944ae1a146e571
Brian <briantcheung@gmail.com>
2024-10-24 02:13:04 -0700

    Adjust transition time

--- src/helpers/zoomImageOnHover.ts
+++ src/helpers/zoomImageOnHover.ts
@@ -2,11 +2,11 @@ export default function zoomImageOnHover(direction: "enter" | "leave", id: strin
     let image = document.getElementById(id)
     if (image !== null) {
         if (direction === "enter") {
-            image.style.transition = "all 100ms ease-in"
+            image.style.transition = "all 125ms ease-in"
             image.style.transform = `scale(1.1) ${translate}`
         }
         else {
-            image.style.transition = "all 100ms ease-out"
+            image.style.transition = "all 125ms ease-out"
             image.style.transform = `scale(1) ${translate}`
         }
     }
