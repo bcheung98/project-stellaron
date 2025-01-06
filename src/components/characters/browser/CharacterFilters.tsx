00d41039b1f5d22be2bafdfcbed0ffba6c595f40
Brian <briantcheung@gmail.com>
2025-01-06 15:43:52 -0800

    Revamp character browser

---
+++ src/components/characters/browser/CharacterFilters.tsx
@@ -0,0 +1,219 @@
+import { BaseSyntheticEvent } from "react";
+
+// Component imports
+import Dropdown from "custom/Dropdown";
+import Image from "custom/Image";
+import ToggleButtons from "custom/ToggleButtons";
+import RarityStars from "custom/RarityStars";
+
+// MUI imports
+import { useTheme, List, IconButton, Toolbar, Button } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import RestartAltIcon from "@mui/icons-material/RestartAlt";
+
+// Helper imports
+import { objectKeys } from "helpers/utils";
+import { useAppDispatch, useAppSelector } from "helpers/hooks";
+import {
+    activeCharacterFilters,
+    clearFilters,
+    selectCharacterFilters,
+    setBossMat,
+    setCalyxMat,
+    setCommonMat,
+    setElement,
+    setPath,
+    setRarity,
+    setWeeklyBossMat,
+    setWorld,
+} from "reducers/characterFilters";
+import { elements, paths, rarities, worlds } from "data/common";
+import { calyxMaterials } from "data/materials/calyxMaterials";
+import { commonMaterials } from "data/materials/commonMaterials";
+import { bossMatNames } from "data/materials/bossMaterials";
+import {
+    formatWeeklyBossMaterials,
+    weeklyBossMatNames,
+} from "data/materials/weeklyBossMaterials";
+
+// Type imports
+import { Element, Path, Rarity, World } from "types/_common";
+import {
+    BossMaterial,
+    CalyxMaterial,
+    CommonMaterial,
+    WeeklyBossMaterial,
+} from "types/materials";
+
+function CharacterFilters({
+    handleClose,
+}: {
+    handleClose: (arg0: any) => void;
+}) {
+    const theme = useTheme();
+
+    const filters = useAppSelector(selectCharacterFilters);
+    const dispatch = useAppDispatch();
+
+    const filterGroups = [
+        {
+            name: "Combat Type",
+            value: filters.element,
+            onChange: (_: BaseSyntheticEvent, newValues: Element[]) =>
+                dispatch(setElement(newValues)),
+            buttons: createButtons<Element>(elements, "elements"),
+        },
+        {
+            name: "Path",
+            value: filters.path,
+            onChange: (_: BaseSyntheticEvent, newValues: Path[]) =>
+                dispatch(setPath(newValues)),
+            buttons: createButtons<Path>(paths, "paths"),
+        },
+        {
+            name: "Rarity",
+            value: filters.rarity,
+            onChange: (_: BaseSyntheticEvent, newValues: Rarity[]) =>
+                dispatch(setRarity(newValues)),
+            buttons: rarities.slice(0, -3).map((rarity) => ({
+                value: rarity,
+                label: <RarityStars rarity={rarity} variant="h6-styled" />,
+            })),
+        },
+        {
+            name: "Calyx Material",
+            value: filters.calyxMat,
+            onChange: (_: BaseSyntheticEvent, newValues: CalyxMaterial[]) =>
+                dispatch(setCalyxMat(newValues)),
+            buttons: createButtons<CalyxMaterial>(
+                objectKeys(calyxMaterials),
+                "materials/calyx"
+            ),
+        },
+        {
+            name: "Common Material",
+            value: filters.commonMat,
+            onChange: (_: BaseSyntheticEvent, newValues: CommonMaterial[]) =>
+                dispatch(setCommonMat(newValues)),
+            buttons: createButtons<CommonMaterial>(
+                objectKeys(commonMaterials),
+                "materials/common"
+            ),
+        },
+        {
+            name: "Boss Material",
+            value: filters.bossMat,
+            onChange: (_: BaseSyntheticEvent, newValues: BossMaterial[]) =>
+                dispatch(setBossMat(newValues)),
+            buttons: createButtons<BossMaterial>(
+                bossMatNames.slice(1),
+                "materials/boss"
+            ),
+        },
+        {
+            name: "Weekly Boss Material",
+            value: filters.weeklyBossMat,
+            onChange: (
+                _: BaseSyntheticEvent,
+                newValues: WeeklyBossMaterial[]
+            ) => dispatch(setWeeklyBossMat(newValues)),
+            buttons: createButtons<WeeklyBossMaterial>(
+                weeklyBossMatNames,
+                "materials/weekly"
+            ),
+        },
+        {
+            name: "World",
+            value: filters.world,
+            onChange: (_: BaseSyntheticEvent, newValues: World[]) =>
+                dispatch(setWorld(newValues)),
+            buttons: createButtons<World>(worlds, "factions"),
+        },
+    ];
+
+    return (
+        <>
+            <Toolbar sx={{ justifyContent: "space-between" }}>
+                <Button
+                    onClick={() => dispatch(clearFilters())}
+                    disabled={!useAppSelector(activeCharacterFilters)}
+                    variant="contained"
+                    color="secondary"
+                    disableElevation
+                    startIcon={<RestartAltIcon />}
+                    sx={{
+                        height: "32px",
+                        "&.Mui-disabled": {
+                            opacity: 0.35,
+                            color: theme.appbar.color,
+                        },
+                    }}
+                >
+                    Reset
+                </Button>
+                <IconButton
+                    onClick={handleClose}
+                    sx={{ color: theme.appbar.color }}
+                >
+                    <CloseIcon />
+                </IconButton>
+            </Toolbar>
+            <List sx={{ px: "16px" }}>
+                {filterGroups.map((filter, index) => (
+                    <Dropdown
+                        key={index}
+                        title={filter.name}
+                        titleColor={
+                            filter.value.length > 0
+                                ? theme.text.selected
+                                : theme.appbar.color
+                        }
+                        contentPadding="4px 0px 4px 24px"
+                    >
+                        <ToggleButtons
+                            color="secondary"
+                            buttons={filter.buttons}
+                            value={filter.value}
+                            onChange={filter.onChange}
+                            spacing={4}
+                            padding={"label" in filter.buttons[0] ? "0 8px" : 0}
+                        />
+                    </Dropdown>
+                ))}
+            </List>
+        </>
+    );
+}
+
+export default CharacterFilters;
+
+function createButtons<T>(items: readonly T[], url: string) {
+    //  const padding = url.startsWith("materials/") ? "0px" : "4px";
+    return items.map((item) => ({
+        value: item,
+        icon: url && (
+            <Image
+                src={`${url}/${item}${
+                    ["materials/calyx", "materials/common"].includes(url)
+                        ? "3"
+                        : ""
+                }`}
+                alt={`${item}`}
+                style={{ width: "32px", padding: "4px", borderRadius: "4px" }}
+                tooltip={getTooltip(item, url)}
+            />
+        ),
+    }));
+}
+
+function getTooltip<T>(item: T, url: string) {
+    let tooltip;
+    if (url.startsWith("materials/weekly")) {
+        tooltip = `${formatWeeklyBossMaterials(item as WeeklyBossMaterial)}`;
+    } else if (url.startsWith("ranks")) {
+        tooltip = "";
+    } else {
+        tooltip = `${item}`;
+    }
+    return tooltip;
+}
---
+++ src/components/characters/browser/CharacterTableRow.tsx
@@ -0,0 +1,135 @@
+// Component imports
+import Image from "custom/Image";
+import RarityStars from "custom/RarityStars";
+import RouterLink from "components/nav/RouterLink";
+import { StyledTableRow, StyledTableCell } from "styled/StyledTable";
+import { TextStyled } from "styled/StyledTypography";
+import { FlexBox } from "styled/StyledBox";
+
+// MUI imports
+import { useTheme } from "@mui/material";
+
+// Helper imports
+import { useAppSelector } from "helpers/hooks";
+import { selectServer } from "reducers/settings";
+import { createDateObject } from "helpers/dates";
+import { combineStyles } from "helpers/utils";
+
+// Type imports
+import { CharacterRow } from "./CharacterTable";
+
+interface CharacterTableRowProps extends CharacterRow {
+    releaseDate: string;
+    version: string;
+}
+
+function CharacterTableRow({ row }: { row: CharacterTableRowProps }) {
+    const theme = useTheme();
+
+    const region = useAppSelector(selectServer);
+
+    const columns = [
+        {
+            label: row.fullName,
+            labelStyle: {
+                cursor: "pointer",
+                "&:hover": {
+                    color: theme.text.selected,
+                    textDecoration: "underline",
+                },
+            },
+            img: `characters/icons/${row.name}`,
+            imgStyle: {
+                width: "48px",
+                height: "auto",
+                borderRadius: "4px",
+                cursor: "pointer",
+            },
+            href: `/characters/${row.name.split(" ").join("_").toLowerCase()}`,
+        },
+        {
+            label: <RarityStars rarity={row.rarity} variant="h4-styled" />,
+        },
+        {
+            label: row.element,
+            img: `elements/${row.element}`,
+        },
+        {
+            label: row.path,
+            img: `paths/${row.path}`,
+        },
+        {
+            label: row.world,
+            img: `factions/${row.world}`,
+        },
+        {
+            label: `${
+                createDateObject({ date: row.releaseDate, region: region }).date
+            } (${row.version})`,
+            labelStyle: { marginLeft: "0px" },
+        },
+    ];
+
+    return (
+        <StyledTableRow color="secondary" hover>
+            {columns.map((col, index) => (
+                <StyledTableCell key={index} sx={{ maxWidth: "275px" }}>
+                    <FlexBox columnGap="16px">
+                        {col.img &&
+                            (col.href ? (
+                                <RouterLink to={col.href}>
+                                    <Image
+                                        src={col.img}
+                                        alt={col.label}
+                                        style={combineStyles(
+                                            {
+                                                width: "32px",
+                                                height: "32px",
+                                            },
+                                            col.imgStyle
+                                        )}
+                                    />
+                                </RouterLink>
+                            ) : (
+                                <Image
+                                    src={col.img}
+                                    alt={col.label}
+                                    style={combineStyles(
+                                        {
+                                            width: "32px",
+                                            height: "32px",
+                                        },
+                                        col.imgStyle
+                                    )}
+                                />
+                            ))}
+                        {col.label &&
+                            (col.href ? (
+                                <RouterLink to={col.href}>
+                                    <TextStyled
+                                        sx={combineStyles(
+                                            { textAlign: "left" },
+                                            col.labelStyle
+                                        )}
+                                    >
+                                        {col.label}
+                                    </TextStyled>
+                                </RouterLink>
+                            ) : (
+                                <TextStyled
+                                    sx={combineStyles(
+                                        { textAlign: "left" },
+                                        col.labelStyle
+                                    )}
+                                >
+                                    {col.label}
+                                </TextStyled>
+                            ))}
+                    </FlexBox>
+                </StyledTableCell>
+            ))}
+        </StyledTableRow>
+    );
+}
+
+export default CharacterTableRow;
